31ab68f88950dc4ce8edf23d5d10f14f40dba88b
unknown <reemalharrasi00@gmail.com>
2026-08-19 08:51:49 +0400

    feat(entities): Person attributes + full constructor [Task 1.1]

---
+++ HospitalManagementSystem/src/main/java/entities/Person.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Person {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String nationalId;
+    private int age;
+    private boolean activeStatus;
+
+    public Person(String id, String firstName, String lastName, String dateOfBirth, String gender,
+              String phoneNumber, String email, String address, String nationalId, int age, boolean activeStatus) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.nationalId = nationalId;
+        this.age = age;
+        this.activeStatus = activeStatus;
+    }
+}
