b4f6c1e6a2b439c2ca006268fce7b4d40d2f47e9
unknown <reemalharrasi00@gmail.com>
2026-08-20 10:35:12 +0400

    feat(services): addPatient overloads (x3) [Task 2.2]

---
+++ HospitalManagementSystem/src/main/java/services/PatientService.java
@@ -0,0 +1,54 @@
+package services;
+
+import entities.Patient;
+import entities.InPatient;
+
+public class PatientService {
+    private Patient[] patients;
+    private int count;
+
+    public PatientService() {
+        patients = new Patient[100];
+        count = 0;
+    }
+
+    /*
+    * public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender,
+                   String phoneNumber, String email, String address, String nationalId, int age,
+                   boolean activeStatus , String bloodGroup, String emergencyContact,String registrationDate,
+                   double outstandingBalance, boolean isInsured) {
+
+* */
+    public Patient addPatient(String firstName, String lastName, int age){
+        String id = "PA"+count;
+        Patient p = new Patient(id,firstName,lastName, "1950-01-01",
+                "other", "+968 0000 0000","unknown",
+                "unknown","unknown", age, true,
+                "unknown","not givin",
+                "2026-08-20",0,false);
+        if (count>=patients.length){
+            System.out.println("List is full");
+        }else{
+            patients[count]=p;
+            count++;
+        }
+        return p;
+
+    }
+
+    public Patient addPatient(String firstName, String lastName, int age, String bloodGroup){
+        Patient p =addPatient(firstName,lastName,age);
+        p.setBloodGroup(bloodGroup);
+        return p;
+    }
+    public Patient addPatient(Patient p){
+        p.setId("PA"+count);
+        if (count>=patients.length){
+            System.out.println("List is full");
+        }else{
+            patients[count]=p;
+            count++;
+        }
+        return p;
+    }
+}
