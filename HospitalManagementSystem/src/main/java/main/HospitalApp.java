34a9fd2de0085e994d1261787a61de9103746a17
unknown <reemalharrasi00@gmail.com>
2026-08-20 16:40:05 +0400

    feat(main): menu loop skeleton + exit [Task 2.8]

--- HospitalManagementSystem/src/main/java/main/HospitalApp.java
+++ HospitalManagementSystem/src/main/java/main/HospitalApp.java
@@ -2,22 +2,16 @@ package main;
 
 import entities.*;
 import utils.HelperUtils;
+import utils.InputHandler;
 
 public class HospitalApp {
     private Person[] people = new Person[100];
     private int count;
 
-    void main(){
-        //people[count++] means add person at position count, then increment count by 1
-        people[count++]=new Person(HelperUtils.generateId("P"),"Person","1");
-        people[count++]= new Patient(HelperUtils.generateId("PA"), "patient", "1", "1998-07-20", "Female", "91234567", "unknown", "Muscat", "123456789", 28, true, "A+", "92345678", "2026-08-20", 75.50, true);
-        people[count++]=new Doctor(HelperUtils.generateId("DR"),"Doctor","1","2000-01-01","Female","9093 9430","Dr@hospital.om","Ghala","12321",26,true,"Kids",5,234.5,true);
-        people[count++]= new Nurse(HelperUtils.generateId("NR"), "nurse", "1", "1995-05-10", "Female", "99999999", "nurse@hospital.om", "Muscat", "12345678", 31, true, "D01", "Morning", 5);
-        people[count++]= new InPatient(HelperUtils.generateId("IPA"), "inpatient", "1", "1995-05-10", "Male", "91234567", "unknown", "Muscat", "123456789", 31, true, "O+", "98765432", "2026-08-20", 150.50, true, "2026-08-20", "R101", 25.0, 5);
-        people[count++]= new Surgeon(HelperUtils.generateId("SR"), "surgeon", "1", "1985-03-15", "Male", "92345678", "surgeon@email.com", "Muscat", "123456789", 41, true, "heart surgery", 15, 50.0, true, 120, true);
+    private InputHandler in = new InputHandler();
 
-        printAll();
-        countByType();
+    void main(){
+        hospitalSystem();
     }
 
     public void printAll() {
@@ -63,4 +57,39 @@ public class HospitalApp {
         return oldest;
     }
 
+    public void hospitalSystem(){
+        boolean running=true;
+        while (running) {
+            System.out.println();
+            System.out.println("======= Hospital System ========");
+            System.out.println("1. Patients ");
+            System.out.println("2. Doctors ");
+            System.out.println("3. Nurses ");
+            System.out.println("4. Appointments ");
+            System.out.println("5. Medical Records");
+            System.out.println("6. Exit");
+            int option = in.readInt("Choose: ",1,6);
+
+            switch (option){
+                case 1:
+                    //patientMenu();
+                    break;
+                case 2:
+                    //doctorMenu();
+                    break;
+                case 3:
+                    //nurseMenu();
+                    break;
+                case 4:
+                    //appointmentMenu();
+                    break;
+                case 5:
+                    //recordMenu();
+                    break;
+                case 6:
+                    System.out.println("GoodBye~");
+                    break;
+            }
+        }
+    }
 }
