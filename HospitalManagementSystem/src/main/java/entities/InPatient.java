307ac46fef8e7634e61db84c52ad7f27d96b6b79
unknown <reemalharrasi00@gmail.com>
2026-08-20 08:35:09 +0400

    feat(entities): InPatient extends Patient + fields [Task 2.1]

--- HospitalManagementSystem/src/main/java/entities/InPatient.java
+++ HospitalManagementSystem/src/main/java/entities/InPatient.java
@@ -10,13 +10,44 @@ public class InPatient extends Patient {
                      String gender, String phoneNumber, String email, String address,
                      String nationalId, int age, boolean activeStatus, String bloodGroup,
                      String emergencyContact, String registrationDate, double outstandingBalance,
-                     boolean isInsured) {
+                     boolean isInsured, String admissionDate , String roomNumber,double dailyCharges,
+                     int daysAdmitted) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, activeStatus, bloodGroup, emergencyContact, registrationDate, outstandingBalance, isInsured);
+        setAdmissionDate(admissionDate);
+        setRoomNumber(roomNumber);
+        setDailyCharges(dailyCharges);
+        setDaysAdmitted(daysAdmitted);
     }
+
+    //setters
+    public void setAdmissionDate(String admissionDate) {
+        if (admissionDate != null && !admissionDate.isEmpty()) this.admissionDate = admissionDate;
+        else System.out.println("admission Date can not be empty");
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        if (roomNumber != null && !roomNumber.isEmpty()) this.roomNumber = roomNumber;
+        else System.out.println("room Number can not be empty");
+    }
+
+    public void setDailyCharges(double dailyCharges) {
+        if (dailyCharges>=0) this.dailyCharges = dailyCharges;
+        else System.out.println("dailyCharges can not be negative");
+
+    }
+
+    public void setDaysAdmitted(Integer daysAdmitted) {
+        if (daysAdmitted>=0) this.daysAdmitted = daysAdmitted;
+        else System.out.println("days Admitted can not be negative");
+    }
+
+    //getters
 
     @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("[Patient details]");
     }
+
+
 }
