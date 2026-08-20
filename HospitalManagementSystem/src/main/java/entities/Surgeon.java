656f327b65860058a8011fc65ad8b07b7c43abf2
unknown <reemalharrasi00@gmail.com>
2026-08-20 09:38:29 +0400

    feat(entities): Surgeon surgery methods [Task 2.1]

--- HospitalManagementSystem/src/main/java/entities/Surgeon.java
+++ HospitalManagementSystem/src/main/java/entities/Surgeon.java
@@ -31,4 +31,38 @@ public class Surgeon extends Doctor{
     public boolean isOperationTheatreAccess() {return operationTheatreAccess;}
     public String[] getUpcomingSurgeries() {return upcomingSurgeries;}
     public int getSurgeryCount(){return surgeryCount;}
+
+    //methods
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("[Surgeon details]");
+        System.out.println("Number of surgeriesPerformed: "+getSurgeriesPerformed());
+        System.out.println("operation theatre access: "+isOperationTheatreAccess());
+        System.out.println("Upcoming surgeries: "+getSurgeryCount());
+    }
+
+    public void performSurgery(){
+        setSurgeriesPerformed(getSurgeryCount()+1);
+    }
+
+    public void scheduleSurgery(String upcoming){
+        //ensure input is not empty
+        if (upcoming.isEmpty()){
+            System.out.println("Invalid: slot can not be empty.");
+            return;
+        }
+
+        //check if list is full
+        if (surgeryCount >= upcomingSurgeries.length) {
+            System.out.println("Invalid: slot time list is full.");
+            return;
+        }
+        //add to list
+        upcomingSurgeries[surgeryCount] = upcoming;
+        surgeryCount++;
+    }
+
+    public int getUpcomingCount(){return this.surgeryCount;}
+
 }
